5caabbdeb081357dfd7b66f489dbdc4e59bc3456
Josemanue2006 <139252240+Josemanue2006@users.noreply.github.com>
2023-07-18 09:24:53 -0600

    Add files via upload

---
+++ tarea.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){int numero;
+cout<<"ingrese un numero";
+cin>> numero;
+while (numero<=100)
+{ cout<<"ingrese un numero";
+cin>>numero;
+}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
